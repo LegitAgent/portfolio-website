0c32050b1fde28e8e5c10505dab5708b1ac8a686
Alba Martin <albamartindarius@gmail.com>
2026-06-02 18:41:58 +0800

    added certificates display

--- portfolio/src/pages/Professional/Certificates.tsx
+++ portfolio/src/pages/Professional/Certificates.tsx
@@ -1,24 +1,28 @@
 import './Certificates.css';
 import { CLOUDFLARE_GATEWAY } from '../../imports/constants.ts';
-import ProjectsDisplay from '../../components/ProjectsDisplay/ProjectsDisplay.tsx';
+import CertificateDisplay from '../../components/CertificateDisplay/CertificateDisplay.tsx';
 import LoadingScreen from '../Misc/LoadingScreen.tsx';
 import ErrorScreen from '../Misc/ErrorScreen.tsx';
 
 import { useState, useEffect } from 'react';
 
 export interface Certificate {
-  project_name: string;
-  project_description: string;
-  project_github: string;
-  project_img_url: string;
-  pArticle_slug: string;
+  id: number;
+  title: string;
+  issuer: string;
+  completion_date: string;
+  credential_url: string | null;
+  certificate_url: string;
+  image_alt: string;
+  skills: string;
+  image_url: string;
 }
 
 interface CertificateResponse {
-  results: Certificate[];
+  certificates: Certificate[];
 }
 
-const projectGatewayURL = CLOUDFLARE_GATEWAY + 'api/db/projects'; // path to project db
+const projectGatewayURL = CLOUDFLARE_GATEWAY + 'api/db/certificates'; // path to certificates db
 
 function Certificates() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -50,15 +54,15 @@ function Certificates() {
   if(isLoading) {
     return (<LoadingScreen />);
   }
-  
+
   return (
     <>
-      <div className="flex items-center justify-center text-7xl">Projects</div>
-      <div className='projectBox'>
-        <div className="projectList">
-          {certificates?.results?.map((certificateStuff) => {
+      <div className="flex items-center justify-center text-7xl">Certificates</div>
+      <div className='certificateBox'>
+        <div className='certificateList'>
+          {certificates?.certificates?.map((certificateStuff) => {
             return (
-              <ProjectsDisplay key={certificateStuff.project_name} project={certificateStuff}/>
+              <CertificateDisplay key={certificateStuff.id} certificate={certificateStuff}/>
             );
           })}
 
