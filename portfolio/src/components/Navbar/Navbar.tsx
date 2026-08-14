1fc3a41c5cd0f250a775e0fc3decd1d40a87b501
Martin Darius Alba <albamartindarius@gmail.com>
2026-08-14 21:50:47 +0800

    feat: added tooltips to navbar icons

--- portfolio/src/components/Navbar/Navbar.tsx
+++ portfolio/src/components/Navbar/Navbar.tsx
@@ -139,7 +139,7 @@ function Navbar({ isCollapsed, onCollapsedChange, selectedBackground, onBackgrou
         aria-label='Primary navigation'
         inert={isCollapsed}
       >
-        <Link className={location.pathname === '/' ? 'navbar__iconLink is-active' : 'navbar__iconLink'} to='/' aria-label='Home'>
+        <Link className={location.pathname === '/' ? 'navbar__iconLink is-active' : 'navbar__iconLink'} to='/' aria-label='Home' data-tooltip='Home'>
           <svg viewBox='0 0 24 24' aria-hidden='true'>
             <path d='M12 3.5 4 10v10.5h5.5v-6h5v6H20V10l-8-6.5Z' />
           </svg>
@@ -152,6 +152,7 @@ function Navbar({ isCollapsed, onCollapsedChange, selectedBackground, onBackgrou
             className={showProfessionalDropdown || isProfessionalActive ? 'navbar__iconLink focus-anim is-active' : 'navbar__iconLink'}
             type='button'
             aria-label='Professional pages'
+            data-tooltip='Professional'
             aria-expanded={showProfessionalDropdown}
             aria-controls={professionalMenuId}
             onClick={() => {
@@ -173,19 +174,19 @@ function Navbar({ isCollapsed, onCollapsedChange, selectedBackground, onBackgrou
             centerOnMobile
           />
         </div>
-        <Link className={isPathActive('/stats') ? 'navbar__iconLink is-active' : 'navbar__iconLink'} to='/stats' aria-label='Statistics'>
+        <Link className={isPathActive('/stats') ? 'navbar__iconLink is-active' : 'navbar__iconLink'} to='/stats' aria-label='Statistics' data-tooltip='Statistics'>
           <svg viewBox='0 0 24 24' aria-hidden='true'>
             <path d='M5.25 19.5A1.25 1.25 0 0 1 4 18.25v-5.5a1.25 1.25 0 0 1 2.5 0v5.5c0 .69-.56 1.25-1.25 1.25Zm6.75 0a1.25 1.25 0 0 1-1.25-1.25V8.75a1.25 1.25 0 0 1 2.5 0v9.5A1.25 1.25 0 0 1 12 19.5Zm6.75 0a1.25 1.25 0 0 1-1.25-1.25V4.75a1.25 1.25 0 0 1 2.5 0v13.5a1.25 1.25 0 0 1-1.25 1.25Z' />
           </svg>
         </Link>
-        <Link className={isPathActive('/contacts') ? 'navbar__iconLink is-active' : 'navbar__iconLink'} to='/contacts' aria-label='Contact'>
+        <Link className={isPathActive('/contacts') ? 'navbar__iconLink is-active' : 'navbar__iconLink'} to='/contacts' aria-label='Contact' data-tooltip='Contact'>
           <svg viewBox='0 0 24 24' aria-hidden='true'>
             <path d='M4 6.75A1.75 1.75 0 0 1 5.75 5h12.5A1.75 1.75 0 0 1 20 6.75v10.5A1.75 1.75 0 0 1 18.25 19H5.75A1.75 1.75 0 0 1 4 17.25V6.75Zm1.78-.25L12 11.26l6.22-4.76H5.78ZM18.5 8l-6.04 4.62a.75.75 0 0 1-.92 0L5.5 8v9.25c0 .14.11.25.25.25h12.5c.14 0 .25-.11.25-.25V8Z' />
           </svg>
         </Link>
         </div>
         <div className='navbar__bottomLinks'>
-          <Link className={isPathActive('/sandbox') ? 'navbar__iconLink is-active' : 'navbar__iconLink'} to='/sandbox' aria-label='Sandbox'>
+          <Link className={isPathActive('/sandbox') ? 'navbar__iconLink is-active' : 'navbar__iconLink'} to='/sandbox' aria-label='Sandbox' data-tooltip='Sandbox'>
             <svg viewBox='0 0 24 24' aria-hidden='true'>
               <path d='M4 7.5 12 4l8 3.5v9L12 20l-8-3.5v-9Zm8-1.86L6.1 8.22 12 10.8l5.9-2.58L12 5.64Zm-6.5 3.72v6.16l5.75 2.52v-6.16L5.5 9.36Zm7.25 8.68 5.75-2.52V9.36l-5.75 2.52v6.16Z' />
             </svg>
@@ -195,6 +196,7 @@ function Navbar({ isCollapsed, onCollapsedChange, selectedBackground, onBackgrou
               className={showBackgroundDropdown ? 'navbar__iconLink focus-anim is-active' : 'navbar__iconLink'}
               type='button'
               aria-label='Choose background'
+              data-tooltip='Background'
               aria-expanded={showBackgroundDropdown}
               aria-controls={backgroundMenuId}
               onClick={() => {
@@ -221,6 +223,7 @@ function Navbar({ isCollapsed, onCollapsedChange, selectedBackground, onBackgrou
           className='navbar__collapseButton'
           type='button'
           aria-label='Hide navigation'
+          data-tooltip='Hide navigation'
           aria-controls='primary-navigation'
           onClick={hideNavbar}
           ref={collapseButtonRef}
@@ -234,6 +237,7 @@ function Navbar({ isCollapsed, onCollapsedChange, selectedBackground, onBackgrou
         className={isCollapsed ? 'navbar__revealButton is-visible' : 'navbar__revealButton'}
         type='button'
         aria-label='Show navigation'
+        data-tooltip='Show navigation'
         aria-controls='primary-navigation'
         aria-expanded={!isCollapsed}
         tabIndex={isCollapsed ? 0 : -1}
